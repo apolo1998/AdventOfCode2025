28bad44a1e74ebde2e7107d624a8132499698ccd
apolo1998 <paoloortlinghaus@gmail.com>
2025-12-04 14:12:49 +0100

    Day 4 finished

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -10,11 +10,11 @@
 //     ".@@@@@@@@.\n" +
 //     "@.@.@@@.@.";
 var file = File.ReadAllText("C:\\SWK5\\Advent of Code\\Day4\\input.txt");
-//var file = "99";
+
 List<string> content = file.Split('\n').ToList();
 int rows = content.Count;
 int cols = content.Max(l => l.Length);
-
+List<Tuple<int,int>> toRemove= new List<Tuple<int, int>>();
 char[,] matrix = new char[rows, cols];
 
 for (int r = 0; r < rows; r++)
@@ -27,40 +27,81 @@ for (int r = 0; r < rows; r++)
 }
 
 int sum = 0;
+bool itemsToRemove = true;
+while (itemsToRemove)
+{
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            char center = matrix[r, c];
+            if (center != '@')
+                continue;
+            LessThan4PaperRoll(r, c);
+        
+        }
+    }
+
+    itemsToRemove = RemoveRolls();
+}
+
+
 for (int r = 0; r < rows; r++)
 {
     for (int c = 0; c < cols; c++)
     {
-        char center = matrix[r, c];
-        if (center != '@')
-            continue;
-        if (lessThan4PaperRoll(r,c, rows,cols))
-        {
-            sum++;
-        }
+        Console.Write(" "+matrix[r, c] +" ");
+        sum += matrix[r, c] == 'X' ? 1 : 0;
     }
+    Console.WriteLine();
 }
-Console.WriteLine(sum);
 
-bool lessThan4PaperRoll(int row, int col, int rows, int cols)
+Console.WriteLine(sum);
+bool RemoveRolls()
+{
+    bool hasItems = toRemove.Count > 0;
+    foreach (var t in toRemove)
+    {
+        matrix[t.Item1, t.Item2] = 'X';
+    }
+    toRemove.Clear();
+    return hasItems;
+}
+void LessThan4PaperRoll(int row, int col)
 {
     int paper = 0;
 
-    void check(int r, int c)
+    void Check(int r, int c)
     {
         if (r >= 0 && r < rows && c >= 0 && c < cols)
-            paper += matrix[r, c] == '@' ? 1 : 0;
+            if (matrix[r, c] == '@')
+            {
+                paper++;
+            }
+    }
+    void ToRemove(int r, int c)
+    {
+        if (matrix[r, c] == '@')
+        {
+            var tuple = new Tuple<int, int>(r, c);
+            toRemove.Add(tuple);
+        }
     }
 
-    check(row + 1, col + 1);
-    check(row + 1, col);
-    check(row + 1, col - 1);
-    check(row - 1, col + 1);
-    check(row - 1, col);
-    check(row - 1, col - 1);
-    check(row, col + 1);
-    check(row, col - 1);
-
-    return paper < 4;
+    Check(row + 1, col + 1);
+    Check(row + 1, col);
+    Check(row + 1, col - 1);
+    Check(row - 1, col + 1);
+    Check(row - 1, col);
+    Check(row - 1, col - 1);
+    Check(row, col + 1);
+    Check(row, col - 1);
+    bool lessThan4Neighbours = paper < 4;
+    
+    if (lessThan4Neighbours)
+    {
+        Console.WriteLine($"Remove paper at row {row} col {col}");
+        ToRemove(row, col);
+    }
 }
 
